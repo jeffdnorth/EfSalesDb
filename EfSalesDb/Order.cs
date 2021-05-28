499ee9853267062e124a22798cdedfe9f1172144
Jeffrey North <JNorth@tql.com>
2021-05-28 16:00:11 -0400

    entity framework

--- EfSalesDb/Order.cs
+++ EfSalesDb/Order.cs
@@ -16,7 +16,8 @@ namespace EfSalesDb
         public int? CustomerId { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
-
+        //virtual in entity framework means the property is in the class but not in the data base
+        //it is marked with virtual so entity framework knows the difference
         public virtual Customer Customer { get; set; }
         public virtual ICollection<OrderLine> OrderLines { get; set; }
     }
--- EfSalesDb/Program.cs
+++ EfSalesDb/Program.cs
@@ -5,9 +5,12 @@ namespace EfSalesDb
 {
     class Program
     {
+        private static int rowsAffected;
 
         static void Main(string[] args)
         {
+
+            //entity framework is below
             //created an instance of our db context
             var context = new SalesdbContext();
             //all cust returned  does everthing that out get all statement does with less work
@@ -18,6 +21,32 @@ namespace EfSalesDb
             var customersInCincinnati = context.Customers
                      .Where(c => c.City == "Cincinnati")
                      .ToList();
+            
+            //main methods ins, update delete
+            //ins
+            var newCustomer = new Customer()
+            {
+                Name = "tql", City = "Cincinnati", State = "OH", Sales = 400000, Active = true
+            };
+           // context.Customers.Add(newCustomer);
+            //add var takes int and gets a string
+            //var rowsAffected = context.SaveChanges();
+            //update  -will not work as my table does not have  a 43
+            var tql = context.Customers.Find(43);
+           //tql.Name = "Total Quality Logistics";
+            var rowsAffected = context.SaveChanges();
+
+
+            //delete records
+            
+            tql = context.Customers.Find(43);
+            context.Customers.Remove(tql);
+            rowsAffected = context.SaveChanges();
+
+            //read the order and bring back the cust for each order
+            var orders = context.Orders.ToList();
+
+
 
         }
         
